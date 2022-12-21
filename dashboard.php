633d5e55579c401912c8e47ac294ae03581e1a43
Nayeem HR <nayeemhasan042@yahoo.com>
2022-12-21 08:48:59 +0600

    dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $_user_id = $_SESSION['id'] ?? 0;
-if(!$_user_id){
+if (!$_user_id) {
     header("Location: index.php");
 }
 ?>
@@ -17,8 +17,79 @@ if(!$_user_id){
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-<h2>Dashboard</h2>
-<a href="logout.php">Log Out</a>
+<nav class="navbar navbar-expand-md">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">IEM</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Income</a>
+            </li>
+            <li class = "nav-item">
+            <a  class = "nav-link" href = "#">Expense</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Budgets</a>
+            </li>
+        </ul>
+        <button class="dropdown add-btn">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Add Record
+            </a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Income</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Expense</a></li>
+            </ul>
+        </button>
+        <div class="dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <img class="avatar"src="assets/img/logo.jpg" alt="">
+                Md. Hassan
+            </a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Settings</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="logout.php">Log Out</a></li>
+            </ul>
+        </div>
+    </div>
+  </div>
+</nav>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+        <div class="card text-center">
+            <div class="card-header">
+                Transactions
+            </div>
+            <div class="card-body">
+                <div class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-success">Income: $40000</a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-danger">Expense: $20000</a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-primary">Savings: $20000</a>
+                </div>
+                <div id="chart">
+</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer>
+    <div class="copyright text-center">
+        &copy; 2022 Nayeem Hassan | All Rights Reserved
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.10.2/umd/popper.min.js" integrity="sha512-nnzkI2u2Dy6HMnzMIkh7CPd1KX445z38XIu4jG1jGw7x5tSL3VBjE44dY4ihMU1ijAQV930SPM12cCFrB18sVw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.min.js" integrity="sha512-1/RvZTcCDEUjY/CypiMz+iqqtaoQfAITmNSJY17Myp4Ms5mdxPS5UV7iOfdZoxcGhzFbOm6sntTKJppjvuhg4g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/js/all.min.js" integrity="sha512-rpLlll167T5LJHwp0waJCh3ZRf7pO6IT1+LZOhAyP6phAirwchClbTZV3iqL3BMrVxIYRbzGTpli4rfxsCK6Vw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.2/jquery.min.js" integrity="sha512-tWHlutFnuG0C6nQRlpvrEhE4QpkG1nn2MOUMWmUeRePl4e3Aki0VB6W1v3oLjFtd0hVOtRQ9PHpSfN6u6/QXkQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
